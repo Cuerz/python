727102c1ab98b5e60def5efeea4c23a0530b64b3
Sean <84277976+LJH0726@users.noreply.github.com>
2022-02-07 20:39:25 +0800

    Update scapy探测主机存活.py

--- icmp/scapy探测主机存活.py
+++ icmp/scapy探测主机存活.py
@@ -7,7 +7,8 @@ def main():
     ip_id = randint(1,65535)
     icmp_id = randint(1,65535)
     icmp_seq = randint(1,65535)
-    packet = IP(dst="192.168.1.108",ttl=64,id=ip_id)/ICMP(id=icmp_id,seq=icmp_seq)/b'root'
+    #输入目标ip
+    packet = IP(dst="xx.xx.xx.xx",ttl=64,id=ip_id)/ICMP(id=icmp_id,seq=icmp_seq)/b'root'
     result = sr1(packet,timeout=1,verbose=False)
     if result:
         for rcv in result:
